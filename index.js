7efa126facb8ba8d05fb11368510e879c361321f
@maruf45 <armaruf450@gmail.com>
2022-11-07 23:46:53 +0600

    Connecting database

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const cors = require("cors");
 const port = 7000 || process.env.PORT;
@@ -7,10 +9,30 @@ const port = 7000 || process.env.PORT;
 app.use(cors());
 app.use(express.json());
 
-// Create API
-app.get("/", (req, res) => {
-    res.send(`Server is running on ${port} port`);
+// Database Connect
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fkczj.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
+const database = async () => {
+  try {
+
+  } catch {}
+};
+
+database().catch((error) => {
+  console.log(error);
+});
+
+
+// Create API
+app.get("/", (req, res) => {
+  res.send(`Server is running on ${port} port`);
+});
 
 app.listen(port);
