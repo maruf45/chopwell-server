4b2b6c4a85799c88845a4a4f1f15d7759a0a04cf
@maruf45 <armaruf450@gmail.com>
2022-11-10 21:25:03 +0600

    upate review

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const app = express();
 const cors = require("cors");
@@ -17,13 +18,34 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-
+function verifyJwt(req, res, next) {
+  const header = req.headers.authorization;
+  if (!header) {
+    return res.status(401).send({ message: "unauthorize access" });
+  }
+  const token = header.split(" ")[1];
+  jwt.verify(token, process.env.TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(401).send({ message: "unauthorize access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
 const database = async () => {
   try {
     const database = client.db("servicesCollection");
     const blogs = database.collection("blogs");
     const services = database.collection("services");
     const review = database.collection("review");
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.TOKEN_SECRET, {
+        expiresIn: "10d",
+      });
+      res.send({ token });
+    });
     // getting blogs api from database
     app.get("/blogs", async (req, res) => {
       const query = {};
@@ -35,15 +57,21 @@ const database = async () => {
     app.get("/services", async (req, res) => {
       const serviceQuery = parseInt(req.query.size);
       const query = {};
-      if (serviceQuery <= 6) {
+      if (serviceQuery === serviceQuery) {
         const cursor = services.find(query).limit(serviceQuery);
         const result = await cursor.toArray();
-        res.send(result);
+        res.send(result.reverse());
       } else {
         const cursor = services.find(query).limit(3);
         const result = await cursor.toArray();
-        res.send(result);
+        res.send(result.reverse());
       }
+    });
+    app.post("/services", async (req, res) => {
+      const newServices = req.body;
+      console.log(newServices);
+      const result = await services.insertOne(newServices);
+      res.send(result);
     });
     // specific service api
     app.get("/:service/:id", async (req, res) => {
@@ -64,9 +92,13 @@ const database = async () => {
       const query = { food: foodId };
       const cursor = review.find(query);
       const result = await cursor.toArray();
-      res.send(result);
+      res.send(result.reverse());
     });
-    app.get("/my-reviews", async (req, res) => {
+    app.get("/my-reviews", verifyJwt, async (req, res) => {
+      const decoded = req.decoded;
+      if (decoded.email !== req.query.email) {
+        res.status(403).send({ message: "unauthorize access" });
+      }
       let query = {};
       if (req.query.email) {
         query = { email: req.query.email };
@@ -100,13 +132,13 @@ const database = async () => {
         },
       };
       const result = await review.updateOne(filter, updateDoc, options);
-      res.send(result)
+      res.send(result);
     });
   } catch {}
 };
 
 database().catch((error) => {
-  console.log(error);
+  console.log(error.message);
 });
 
 // Create API
