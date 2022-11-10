8e76b39d6a71af5bdebc4719b595d77ea8491204
@maruf45 <armaruf450@gmail.com>
2022-11-10 22:46:46 +0600

    update server site

--- index.js
+++ index.js
@@ -7,12 +7,7 @@ const cors = require("cors");
 const port = 7000 || process.env.PORT;
 
 // Using Middleware
-const corsOptions ={
-  origin:'*', 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200,
-}
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 // Database Connect
